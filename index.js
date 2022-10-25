0d5d28553fb0609ffd1f21ba07fa862f9fdbea07
Safin-Ali <safin.ali.7205@gmail.com>
2022-10-25 21:57:25 +0600

    Learn Programz server side

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+
+const pythonDocsAPI = express();
+
+const PGlangData = require('./pythonAPIData.json')
+
+const port = process.env.PORT || 5000;
+
+pythonDocsAPI.use(cors())
+
+// get all language name fata
+
+pythonDocsAPI.get('/',(req,res)=>{
+    res.send(PGlangData)
+})
+
+// get data categories wise
+
+pythonDocsAPI.get('/category/:uid',(req,res)=>{
+    const params = req.params.uid;
+    const categoryData = PGlangData.find(elm => params.toLowerCase() === elm.name.toLowerCase());
+    res.send(categoryData);
+})
+
+// get data subject/topic/id wise
+
+pythonDocsAPI.get('/:langname/:subtitle',(req,res)=>{
+
+    const subjname = req.params.langname;
+    let i = 0;
+    const getOnlyName = PGlangData.find(elm => subjname.toLowerCase() === elm.name.toLowerCase());
+    const subtitle = req.params.subtitle;
+    const getSingleData = PGlangData[getOnlyName.id].data.find(elm => parseInt(elm.categoryId) === parseInt(subtitle))
+    res.send(getSingleData)
+})
+
+pythonDocsAPI.listen(port,()=>{
+    console.log('This Server Is Running')
+})
