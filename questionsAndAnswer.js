ae51cd3800452ac37ca0fd6f9f62820c66b28831
Safin-Ali <safin.ali.7205@gmail.com>
2022-10-27 06:41:29 +0600

    created questions object to using javascript

---
+++ questionsAndAnswer.js
@@ -0,0 +1,21 @@
+const x = [
+    {
+        QuestionName: 'what is cors?',
+        Answer: 'Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources. CORS also relies on a mechanism by which browsers make a "preflight" request to the server hosting the cross-origin resource, in order to check that the server will permit the actual request. In that preflight, the browser sends headers that indicate the HTTP method and headers that will be used in the actual request.'
+    },
+    {
+        QuestionName: 'Why are you using firebase?',
+        Answer: 'Firebase helps you develop high-quality apps, grow your user base, and earn more money. Each feature works independently, and they work even better together.'
+        
+    },
+    {
+        QuestionName: 'How does the private route work?',
+        Answer: 'The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in).'
+
+    },
+    {
+        QuestionName: 'How does the private route work?',
+        Answer: 'Node.js is an open source server environment,Node.js is free,Node.js runs on various platforms (Windows, Linux, Unix, Mac OS X, etc.),Node.js uses JavaScript on the server'
+
+    },
+]
